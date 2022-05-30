55aa3c2f11b38d26a7f2a0d31e5ec088b102f372
Opartl <32740205+Opartl@users.noreply.github.com>
2022-05-30 11:40:42 +0200

    Add files via upload

---
+++ BreakCamelCase.cs
@@ -0,0 +1,40 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class BreakCamelCase 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(decode("camelCasingTest"));
+        }
+
+        static public string decode(string data)
+        {
+            bool jeUpper = false;
+            var builder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+
+                if (Char.IsUpper(data[i]))
+                {
+                  
+                    builder.Append(' ').Append(data[i]);
+
+
+                }
+                else
+                {
+                    builder.Append(data[i]);
+                }
+
+            }
+            return builder.ToString();
+        }
+    }
+}
+*/
---
+++ CountDuplicates.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class CountDuplicates
+    {
+        /* static void Main(string[] args)
+         {
+              var obj = GenerateDictionary("aadd");
+              foreach (var kvp in obj)
+              {
+                  Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+              }
+
+              string test = "aabb";
+              int pocet = test.Count(char.IsLetterOrDigit);
+
+        Console.WriteLine(Result("aad"));
+
+
+        }*/
+        public static Dictionary<char, int> GenerateDictionary(string text)
+        {
+            var charCounts = new Dictionary<char, int>();
+            foreach (char letter in text)
+            {
+                var lowerLetter = Char.ToLower(letter);
+                if (charCounts.ContainsKey(lowerLetter))
+                {
+                    charCounts[lowerLetter]++;
+                }
+                else
+
+                {
+                    charCounts.Add(lowerLetter, 1);
+                }
+
+
+
+            }
+            return charCounts;
+        }
+
+        public static int Decode(Dictionary<char, int> charCounts)
+        {
+           
+            int count = 0;
+            foreach (var kvp in charCounts)
+            {
+                if (kvp.Value > 1)
+                {
+                    count += 1;
+                }
+
+
+            }
+
+            return count;
+        }
+        public static int Result(string text)
+        {
+            var obj = GenerateDictionary(text);
+
+            return Decode(obj);
+        }
+    }
+}
